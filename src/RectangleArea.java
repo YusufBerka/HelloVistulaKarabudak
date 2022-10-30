3ce70166c13cc08d9f464efa6500f6b12dfef9df
berka <berkayusufk@gmail.com>
2022-10-30 20:51:13 +0100

    cwicenia 4 zadanie 1

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class RectangleArea {
-    double yusufPole, yusufA, yusufB;
+    double yusufArea, yusufA, yusufB;
 
     private void getData() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Mr.karabudak: Enter the length of the first side of the rectangle:");
+        System.out.println("Mr.karabudak: Enter the length of the long side of the rectangle:");
         yusufA = scanner.nextDouble();
-        System.out.println("Mr.karabudak: Enter the length of the other side of the rectangle:");
+        System.out.println("Mr.karabudak:Enter the length of the short side of the rectangle :");
         yusufB = scanner.nextDouble();
 
     }
 
     private void computerField() {
-        yusufPole = yusufA * yusufB;
+        yusufArea = yusufA * yusufB;
     }
 
     public void fieldybkDisplay() {
         getData();
         computerField();
-        System.out.println("Pole prostoką:" + yusufPole);
+        System.out.println("Mr.karabudak : area of this rectangle:" + yusufArea);
     }
 }
